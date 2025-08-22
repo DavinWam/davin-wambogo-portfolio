0e7baebb53762905487da8e04f8967be1732a58b
DavinW <49922027+DavinWam@users.noreply.github.com>
2025-08-22 02:26:58 -0400

    separated game data into json

--- PyTools/config.py
+++ PyTools/config.py
@@ -1,1 +1,4 @@
-TOP_LEVEL_DIR = r"C:\Users\vin50\OneDrive\Documents\davin-wambogo-portfolio\docs"
+from pathlib import Path
+
+TOP_LEVEL_DIR = Path(r"C:\Users\vin50\OneDrive\Documents\davin-wambogo-portfolio\docs")
+GAMES_JSON_PATH = TOP_LEVEL_DIR / "games.json"
